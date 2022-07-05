e0524e6be50b3c36412905f668ee497c8912aeab
Panji Prawiro <prawiro.panji@gmail.com>
2022-07-05 12:22:08 +0700

    fix tampilan laporan laba rugi, menambahkan  conditional array kosong hasil  dari fetch database pada dashboard page

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -10,12 +10,19 @@ async function getLandingPage(req, res) {
   // const sales = await Sale.getAllSale();
 
   let totalIncome = await Laporan.getTotalIncomeByDay(new Date());
-  totalIncome = formatIDCurrency(totalIncome[0].totalPrice);
+
+  if (totalIncome.length > 0) {
+    totalIncome = formatIDCurrency(totalIncome[0].totalPrice);
+  } else {
+    totalIncome = formatIDCurrency(0);
+  }
 
   let countSale = await Laporan.getAmountOfSaleByDay(new Date());
-  countSale = countSale[0].countSale;
-
-  // const saleAmount = sales.length;
+  if (countSale.length > 0) {
+    countSale = countSale[0].countSale;
+  } else {
+    countSale = 0;
+  }
 
   const voidList = await Sale.getVoid();
   const voidAmount = voidList.length;
--- public/scripts/generate-pdf-laba-rugi.js
+++ public/scripts/generate-pdf-laba-rugi.js
@@ -1,5 +1,7 @@
 const titleText = document.getElementById('report-title').textContent;
-const grandTotalText = document.querySelector('.grand-total').textContent;
+const grandTotalText = document.getElementById('income-statement').textContent;
+
+console.log(grandTotalText);
 
 function generatePDF() {
   var doc = new jsPDF('p', '', 'a4');
@@ -20,7 +22,7 @@ function generatePDF() {
     },
   });
 
-  doc.text(grandTotalText, 80, doc.lastAutoTable.finalY + 20);
+  doc.text(grandTotalText, 140, doc.lastAutoTable.finalY + 20);
 
   doc.save(`${titleText}.pdf`);
 }
